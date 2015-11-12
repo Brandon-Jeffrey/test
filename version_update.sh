e2a26ce12684bdd4f8997a8ca496637236d72e74
Brandon Jeffrey <brandon.jeffrey@madbid.com>
2015-11-12 17:56:02 +0000

    now prints contents to console

--- version_update.sh
+++ version_update.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
+
+excluded=($(<excluded_files.txt))
+
+
+
+ls | while read filename ; do
+
+
+
+if ! [[ " ${excluded[@]} " =~ " $filename " ]] ; then
+
+sed "s/#version[^\n]*/#version `echo $1`/g" `echo $filename`
+
+fi
+
+done
+
 sed "s/#version[^\n]*/#version `echo $1`/g" test_file.txt > test_file.txt
 sed "s/#version[^\n]*/#version `echo $1`/g" new_file.txt > new_file.txt
